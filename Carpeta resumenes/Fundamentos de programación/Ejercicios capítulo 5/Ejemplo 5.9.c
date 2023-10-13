4db925d24123332553a8973b197994a5f4c0a503
Welnny Rivas <145089920+Welnny@users.noreply.github.com>
2023-10-13 08:24:29 -0400

    Add files via upload

--- Carpeta resumenes/Fundamentos de programación/Ejercicios capítulo 5/Ejemplo 5.9.c
+++ Carpeta resumenes/Fundamentos de programación/Ejercicios capítulo 5/Ejemplo 5.9.c
@@ -23,4 +23,30 @@ printf("\nEl elemento se encuentra en la posición %d", RES);
 else
 printf("\nEl elemento no se encuentra en el arreglo");
 }
+void Lectura(int A[], int T)
+/* La función Lectura se utiliza para leer un arreglo unidimensional de T elementos de tipo entero. */
+{
+int I;
+for (I=0; I<T; I++)
+{
+printf("Ingrese el elemento %d: ", I+1);
+scanf("%d", &A[I]);
+}
+}
+int Busca(int A[], int T, int K)
+/* Esta función localiza en el arreglo un elemento determinado. Si el elemento es encontrado, regresa la posición correspondiente. En caso contrario, regresa 0. */
+{
+int I = 0, BAN = 0, RES;
+while (I < T && !BAN)
+if (A[I] == K)
+BAN++;
+else
+I++;
+if (BAN)
+RES = I + 1;
+/* Se asigna I+1 dado que las posiciones en el arreglo comienzan desde cero. */
+else
+RES = BAN;
+return (RES);
+}
 
